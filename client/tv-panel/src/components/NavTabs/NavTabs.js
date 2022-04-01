0cbbf2af8c2279915f38ade4a38a7d13f44356ac
imanmansour86 <imanmansour86@gmail.com>
2022-03-31 23:35:58 -0700

    adding reac icons for login icon

--- client/tv-panel/src/components/NavTabs/NavTabs.js
+++ client/tv-panel/src/components/NavTabs/NavTabs.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./styles.css";
+import { FaUser } from "react-icons/fa";
 
 const NavTabs = () => {
   return (
@@ -9,7 +10,12 @@ const NavTabs = () => {
         <div className="right-nav">
           <div className="nav-text">Current Shows</div>
           <div className="nav-text">Coming Soon</div>
-          <div className="nav-text">Sign In</div>
+          <div className="nav-text">
+            <button className="sign-in">
+              {" "}
+              <FaUser /> Sign In
+            </button>
+          </div>
         </div>
       </div>
     </div>
