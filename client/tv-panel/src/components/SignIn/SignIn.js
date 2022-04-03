655dd2d38cc3a7f4ea29053dc538b8026b082623
imanmansour86 <imanmansour86@gmail.com>
2022-04-02 23:28:16 -0700

    styling sign in modal

--- client/tv-panel/src/components/SignIn/SignIn.js
+++ client/tv-panel/src/components/SignIn/SignIn.js
@@ -1,12 +1,39 @@
 import React from "react";
 import "./styles.css";
+import { FaRegTimesCircle } from "react-icons/fa";
+
 const SignIn = () => {
   return (
     <div className="modal">
       <div className="wrapper">
-        <div className="header">Header</div>
-        <div className="content">Content</div>
-        <div className="footer">Footer</div>
+        <div className="close">
+          <FaRegTimesCircle />
+        </div>
+        <div className="header">Sign in with your account</div>
+        <div className="header-sub">Enter credentials below</div>
+        <div className="sign-in-inputs">
+          <input
+            className="sign-input"
+            type="text"
+            name="username"
+            autoComplete="off"
+            autocapitalize="off"
+            required="true"
+            placeholder="Username"
+          ></input>
+          <input
+            className="sign-input"
+            type="password"
+            name="password"
+            autoComplete="off"
+            autocapitalize="off"
+            required="true"
+            placeholder="Password"
+          ></input>
+        </div>
+        <div className="footer">
+          <a href="#">Create new user</a>
+        </div>
       </div>
     </div>
   );
